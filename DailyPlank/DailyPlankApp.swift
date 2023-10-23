f153285863828451588622ed21bda24a843cc5d3
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-10-23 18:50:47 +0200

    Initial Commit

---
+++ DailyPlank/DailyPlankApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyPlankApp.swift
+//  DailyPlank
+//
+//  Created by Ivan Trajanovski on 23.10.23.
+//
+
+import SwiftUI
+
+@main
+struct DailyPlankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
